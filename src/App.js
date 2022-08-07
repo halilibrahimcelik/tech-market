b4c3519b408830293ed14266f302148f572f0099
halilibrahimcelik <halil.celik139@gmail.com>
2022-08-07 17:46:03 +0300

    Added default json data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    fetch("https://dummyjson.com/products/category/laptops")
+      .then((res) => res.json())
+      .then(console.log);
+  }, []);
   return <div className="App"></div>;
 }
 
