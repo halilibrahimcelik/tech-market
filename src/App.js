83935d38f13ab3f90425797058f97ee922b2f1df
halilibrahimcelik <halil.celik139@gmail.com>
2022-08-08 14:28:32 +0300

    Configured Firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { useEffect } from "react";
 import Container from "./components/UI/Container";
 import AppRouter from "./router/AppRouter";
 
 function App() {
-  useEffect(() => {
-    fetch("https://dummyjson.com/products/category/laptops")
-      .then((res) => res.json())
-      .then(console.log);
-  }, []);
   return (
     <Container>
       <AppRouter />
--- src/pages/register/SignUp.jsx
+++ src/pages/register/SignUp.jsx
@@ -8,20 +8,52 @@ import visibilityIcon from "../../assets/eye-solid.svg";
 import inVisibilityIcon from "../../assets/eye-slash-solid.svg";
 import { useState } from "react";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../../helpers/firebase.config";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
   const nameInput = useRef();
   const mailInput = useRef();
   const passwordInput = useRef();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const enteredName = nameInput.current.value;
-    const enteredMail = mailInput.current.value;
-    const enteredPassword = passwordInput.current.value;
 
-    console.log(enteredMail, enteredName, enteredPassword);
+    try {
+      const enteredName = nameInput.current.value;
+      const enteredMail = mailInput.current.value;
+      const enteredPassword = passwordInput.current.value;
+
+      const auth = getAuth();
+      const userCredential = createUserWithEmailAndPassword(
+        auth,
+        enteredMail,
+        enteredPassword
+      );
+      const { user } = await userCredential;
+
+      updateProfile(auth.currentUser, { displayName: enteredName }); //?provide current username
+      const formData = {
+        name: enteredName,
+        email: enteredMail,
+        timeStamp: serverTimestamp(),
+      };
+      const docRef = doc(db, "users", user.uid);
+      await setDoc(docRef, formData);
+
+      navigate("/", { replace: false });
+    } catch (error) {
+      console.log(error);
+      console.log(error.message);
+    }
     e.target.reset();
   };
   return (
